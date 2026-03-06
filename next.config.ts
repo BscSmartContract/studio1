9ed1c80b74212f98055a489badaff3aa63958058
theroasterop1 <theroasterop1@gmail.com>
2026-03-06 20:05:58 +0000

    https://studio1-pi.vercel.app/admin allow the Website to connect to this

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,24 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["studio1-pi.vercel.app", "localhost:9002"],
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://studio1-pi.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
